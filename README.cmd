e3aed81535971e5910bdc38399d7a3297f57f2ce
sunilkumar kalva <skalva404@gmail.com>
2021-11-08 12:58:53 +0530

    First commit

---
+++ README.cmd
@@ -0,0 +1,5 @@
+# **Problem Statement**
+
+Implement a Actor model where user should able to instantiate an Actor system and subscribe the actors with their mail boxes,
+and producer should be able to produce to the designated mai boxes thus actors shall be able to process the messages received by
+their respective mail boxes.
